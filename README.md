3d84adc2f834860d1bb747005c770d01993275dd
kundangupta04 <kundankr1200@outlook.com>
2025-10-04 14:24:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Crypto Price Tracker
+
+A web application to track cryptocurrency prices, market cap, and historical price charts. Built with **React** and powered by the **CoinGecko API**.
+
+## Features
+
+- **Real-time Price Tracking**: Get up-to-date prices for top cryptocurrencies.
+- **Market Cap Overview**: See market capitalization data for popular coins.
+- **Price Charts**: View historical price trends with interactive charts.
+- **Responsive Design**: Works seamlessly on desktop and mobile devices.
+
+
+## Technologies Used
+
+- **React.js** – Frontend library
+- **CoinGecko API** – Cryptocurrency data
+- **Google charts** – Interactive charts
+- **Vercel** – Deployment
+
+## Installation
+
+1. Clone the repo:
+```bash
+git clone https://github.com/yourusername/crypto-price-tracker.git
---
+++ src/context/CoinContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useEffect, useState } from "react";
+
+export const CoinContext = createContext();
+
+const CoinContextProvider = (props)=>{
+
+    const [allCoin, setAllCoin] = useState([]);
+    const [currency, setCurrency] = useState({
+        name: "usd",
+        symbol: "$"
+    })
+
+    const fetchAllCoin = async ()=>{
+        const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`;
+        const options = {method: 'GET', headers: {'x-cg-demo-api-key': 'CG-4Cmy5aeSc7uPMFSKkCSfhxEM'}, body: undefined};
+
+        try {
+            const response = await fetch(url, options);
+            const data = await response.json();
+            console.log(setAllCoin(data));
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+useEffect(()=>{
+    fetchAllCoin();
+},[currency])
+
+    const contextValue = {
+        allCoin, currency, setCurrency
+    }
+
+    return(
+        <CoinContext.Provider value={contextValue}>
+            {props.children}
+        </CoinContext.Provider>
+    )
+}
+
+export default CoinContextProvider;
